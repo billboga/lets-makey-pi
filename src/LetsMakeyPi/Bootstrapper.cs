53ccebd7d0dc7520f9efb7b4095a17bfca417b0e
William Boga IV <williambogaiv@gmail.com>
2015-03-21 16:48:56 -0400

    Add SignalR JS package. Also create custom Nancy bootstrapper. This is needed to map static Scripts directory (related to SignalR JS assets). Add necessary script tags to layout for now.

---
+++ src/LetsMakeyPi/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using Nancy.Conventions;
+
+namespace LetsMakeyPi
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(Nancy.Conventions.NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            nancyConventions
+                .StaticContentsConventions
+                .Add(StaticContentConventionBuilder.AddDirectory("Scripts"));
+        }
+    }
+}
