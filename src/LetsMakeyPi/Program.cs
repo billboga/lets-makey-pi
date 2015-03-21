21a4dfc4a2469e4834306a972fc1527f55bbc729
William Boga IV <williambogaiv@gmail.com>
2015-03-21 13:08:24 -0400

    Add skeleton for running and self-hosting NancyFX.

--- src/LetsMakeyPi/Program.cs
+++ src/LetsMakeyPi/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
 
 namespace LetsMakeyPi
 {
@@ -10,6 +7,14 @@ namespace LetsMakeyPi
     {
         static void Main(string[] args)
         {
+            var url = "http://+:8080";
+
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Running on {0}", url);
+                Console.WriteLine("Press enter to exit");
+                Console.ReadLine();
+            }
         }
     }
 }
